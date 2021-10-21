cbd5c47d1a10afeb989f76f90cf8f59feb18b2b6
miloszKubiak <milypan69@gmail.com>
2021-10-21 11:03:28 +0200

    add create view for the notes

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NoteList, NoteDetail
+from .views import NoteList, NoteDetail, NoteCreate
 
 urlpatterns = [
   path('', NoteList.as_view(), name='notes'),
   path('note/<int:pk>/', NoteDetail.as_view(), name='note'),
+  path('note-create/', NoteCreate.as_view(), name='note-create'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Note
 from django.views.generic.detail import DetailView
-
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 
 class NoteList(ListView):
@@ -14,3 +15,9 @@ class NoteDetail(DetailView):
 	model = Note
 	context_object_name = 'note'
 	template_name = 'base/note.html'
+
+
+class NoteCreate(CreateView):
+	model = Note
+	fields = '__all__'
+	success_url = reverse_lazy('notes')
