a1d48e9e027a5b14bf320f8497007de93c49d6ac
miloszKubiak <milypan69@gmail.com>
2021-10-21 10:43:50 +0200

    create view for the details of a single note

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import NoteList
+from .views import NoteList, NoteDetail
 
 urlpatterns = [
   path('', NoteList.as_view(), name='notes'),
+  path('note/<int:pk>/', NoteDetail.as_view(), name='note'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Note
+from django.views.generic.detail import DetailView
+
+
 
 class NoteList(ListView):
 	model = Note
 	context_object_name = 'notes'
+
+
+class NoteDetail(DetailView):
+	model = Note
+	context_object_name = 'note'
+	template_name = 'base/note.html'
