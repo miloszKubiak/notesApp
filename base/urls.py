03111ca835462c779c56a0a394eb280fb5d3dd7a
miloszKubiak <milypan69@gmail.com>
2021-10-21 12:23:55 +0200

    add login/logout functionality for users

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import NoteDelete, NoteList, NoteDetail, NoteCreate, NoteUpdate
+from .views import NoteDelete, NoteList, NoteDetail, NoteCreate, NoteUpdate, CustomLoginView
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
+  path('login/', CustomLoginView.as_view(), name='login'),
+  path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
   path('', NoteList.as_view(), name='notes'),
   path('note/<int:pk>/', NoteDetail.as_view(), name='note'),
   path('note-create/', NoteCreate.as_view(), name='note-create'),
--- base/views.py
+++ base/views.py
@@ -1,11 +1,22 @@
 from functools import update_wrapper
 from django.forms.models import model_to_dict
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from .models import Note
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+
+
+
+class CustomLoginView(LoginView):
+	template_name = 'base/login.html' 
+	fields = '__all__'
+	redirect_authenticated_user = True
+
+	def get_success_url(self):
+		return reverse_lazy('notes')
 
 
 class NoteList(ListView):
