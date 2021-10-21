3788f52e9e2888a7aff5175c9cc8cff2284aac8f
miloszKubiak <milypan69@gmail.com>
2021-10-21 11:54:17 +0200

    create views for note detail and delete note

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import NoteList, NoteDetail, NoteCreate
+from .views import NoteDelete, NoteList, NoteDetail, NoteCreate, NoteUpdate
 
 urlpatterns = [
   path('', NoteList.as_view(), name='notes'),
   path('note/<int:pk>/', NoteDetail.as_view(), name='note'),
   path('note-create/', NoteCreate.as_view(), name='note-create'),
+  path('note-update/<int:pk>', NoteUpdate.as_view(), name='note-update'),
+  path('note-delete/<int:pk>', NoteDelete.as_view(), name='note-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
+from functools import update_wrapper
+from django.forms.models import model_to_dict
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Note
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
@@ -20,4 +22,16 @@ class NoteDetail(DetailView):
 class NoteCreate(CreateView):
 	model = Note
 	fields = '__all__'
+	success_url = reverse_lazy('notes')
+
+
+class NoteUpdate(UpdateView):
+	model = Note
+	fields = '__all__'
+	success_url = reverse_lazy('notes')
+
+
+class NoteDelete(DeleteView):
+	model = Note
+	context_object_name = 'note'
 	success_url = reverse_lazy('notes')
