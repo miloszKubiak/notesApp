78a3097dece7d87917caef365f2270e84ac28f30
miloszKubiak <milypan69@gmail.com>
2021-10-22 08:41:59 +0200

    add registration for users

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import NoteDelete, NoteList, NoteDetail, NoteCreate, NoteUpdate, CustomLoginView
+from .views import NoteDelete, NoteList, NoteDetail, NoteCreate, NoteUpdate, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
   path('login/', CustomLoginView.as_view(), name='login'),
   path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+  path('register/', RegisterPage.as_view(), name='register'),
   path('', NoteList.as_view(), name='notes'),
   path('note/<int:pk>/', NoteDetail.as_view(), name='note'),
   path('note-create/', NoteCreate.as_view(), name='note-create'),
--- base/views.py
+++ base/views.py
@@ -1,13 +1,13 @@
-from functools import update_wrapper
-from django.forms.models import model_to_dict
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from .models import Note
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 class CustomLoginView(LoginView):
@@ -19,6 +19,24 @@ class CustomLoginView(LoginView):
 		return reverse_lazy('notes')
 
 
+class RegisterPage(FormView):
+	template_name = 'base/register.html'
+	form_class = UserCreationForm
+	redirect_authenticated_user = True
+	success_url = reverse_lazy('notes')
+
+	def form_valid(self, form):
+		user = form.save()
+		if user is not None:
+			login(self.request, user)
+		return super(RegisterPage, self).form_valid(form)
+
+	def get(self, *args, **kwargs):
+		if self.request.user.is_authenticated:
+			return redirect('notes')
+		return super(RegisterPage, self).get(*args, **kwargs)
+
+
 class NoteList(LoginRequiredMixin, ListView):
 	model = Note
 	context_object_name = 'notes'
@@ -27,6 +45,7 @@ class NoteList(LoginRequiredMixin, ListView):
 		context = super().get_context_data(**kwargs)
 		context['notes'] = context['notes'].filter(user=self.request.user)
 		return context
+
 
 class NoteDetail(LoginRequiredMixin, DetailView):
 	model = Note
