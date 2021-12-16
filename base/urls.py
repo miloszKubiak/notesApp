43c89670ff46d4092757d64064a9a2abe2d2e3be
miloszKubiak <milypan69@gmail.com>
2021-12-16 18:47:48 +0100

    fix some bugs and add a new style to the app

--- base/urls.py
+++ base/urls.py
@@ -4,12 +4,12 @@ from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-  path('login/', CustomLoginView.as_view(), name='login'),
-  path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-  path('register/', RegisterPage.as_view(), name='register'),
-  path('', NoteList.as_view(), name='notes'),
-  path('note/<int:pk>/', NoteDetail.as_view(), name='note'),
-  path('note-create/', NoteCreate.as_view(), name='note-create'),
-  path('note-update/<int:pk>', NoteUpdate.as_view(), name='note-update'),
-  path('note-delete/<int:pk>', NoteDelete.as_view(), name='note-delete'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('', NoteList.as_view(), name='notes'),
+    path('note/<int:pk>/', NoteDetail.as_view(), name='note'),
+    path('note-create/', NoteCreate.as_view(), name='note-create'),
+    path('note-update/<int:pk>', NoteUpdate.as_view(), name='note-update'),
+    path('note-delete/<int:pk>', NoteDelete.as_view(), name='note-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -11,74 +11,69 @@ from django.contrib.auth import login
 
 
 class CustomLoginView(LoginView):
-	template_name = 'base/login.html' 
-	fields = '__all__'
-	redirect_authenticated_user = True
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
 
-	def get_success_url(self):
-		return reverse_lazy('notes')
+    def get_success_url(self):
+        return reverse_lazy('notes')
 
 
 class RegisterPage(FormView):
-	template_name = 'base/register.html'
-	form_class = UserCreationForm
-	redirect_authenticated_user = True
-	success_url = reverse_lazy('notes')
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('notes')
 
-	def form_valid(self, form):
-		user = form.save()
-		if user is not None:
-			login(self.request, user)
-		return super(RegisterPage, self).form_valid(form)
-
-	def get(self, *args, **kwargs):
-		if self.request.user.is_authenticated:
-			return redirect('notes')
-		return super(RegisterPage, self).get(*args, **kwargs)
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
 
 
 class NoteList(LoginRequiredMixin, ListView):
-	model = Note
-	context_object_name = 'notes'
+    model = Note
+    context_object_name = 'notes'
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context['notes'] = context['notes'].filter(user=self.request.user)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['notes'] = context['notes'].filter(user=self.request.user)
 
-		search_input = self.request.GET.get('search-area') or ''
-		if search_input:
-			context['notes'] = context['notes'].filter(title__icontains=search_input)
-			# title__startswith - if we want to filter from the first letter
-		
-		context['search_input'] = search_input
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['notes'] = context['notes'].filter(
+                title__icontains=search_input)
 
-		return context
+        context['search_input'] = search_input
+
+        return context
 
 
 class NoteDetail(LoginRequiredMixin, DetailView):
-	model = Note
-	context_object_name = 'note'
-	template_name = 'base/note.html'
+    model = Note
+    context_object_name = 'note'
+    template_name = 'base/note.html'
 
 
 class NoteCreate(LoginRequiredMixin, CreateView):
-	model = Note
-	fields = ['title', 'description']
-	success_url = reverse_lazy('notes')
+    model = Note
+    fields = ['title', 'description']
+    success_url = reverse_lazy('notes')
 
-	def form_valid(self, form):
-		form.instance.user = self.request.user
-		return super(NoteCreate, self).form_valid(form)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(NoteCreate, self).form_valid(form)
 
 
 class NoteUpdate(LoginRequiredMixin, UpdateView):
-	model = Note
-	fields = ['title', 'description']
-	success_url = reverse_lazy('notes')
-	template_name = 'base/note-edit.html'
+    model = Note
+    fields = ['title', 'description']
+    success_url = reverse_lazy('notes')
+    template_name = 'base/note-edit.html'
 
 
 class NoteDelete(LoginRequiredMixin, DeleteView):
-	model = Note
-	context_object_name = 'note'
-	success_url = reverse_lazy('notes')
+    model = Note
+    context_object_name = 'note'
+    success_url = reverse_lazy('notes')
